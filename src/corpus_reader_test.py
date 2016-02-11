69e2dd75d092c7d60ae80f6c4bdc4df9dce87271
Roland Schäfer <roland.schaefer@fu-berlin.de>
2016-02-11 19:50:34 +0100

    minor fixes

--- src/corpus_reader_test.py
+++ src/corpus_reader_test.py
@@ -1,7 +1,11 @@
-#!/usr/bin/python
-
 from cowtop import Cowcorp
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("infile", help="input file in COW-XML")
+args = parser.parse_args()
+
+# Filters on columns. Some remove tokens altogether.
 filters=list()
 filters.append([2, "entities"])
 filters.append([2, "lower"])
@@ -9,14 +13,16 @@ filters.append([2, "length", 4, 20])
 filters.append([2, "copyif2", 0, "(unknown)", 3, "I-PER"])
 filters.append([2, "blacklist", {"(unknown)", "(blank)", "@card@"}])
 filters.append([2, "alpha"])
-
 filters.append([1, "lower"])
 filters.append([1, "truncate", 2])
 filters.append([1, "whitelist", {"nn", "vv", "ad", "ne"}])
 
+# Which annotations to use.
 selectors=[2,1]
 
-c=Cowcorp("cowcat.patch.xml", selectors, filters)
+# Create corpus object.
+c=Cowcorp(args.infile, selectors, filters)
 
+# Just print corpus for demo.
 for l in c:
     print l.encode("utf-8")
